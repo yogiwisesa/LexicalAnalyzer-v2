69a6b882ac6bebba63693066ff169a962ee0ff7c
DESKTOP-EDV0F0U\Yogi Wisesa <yogiwisesa@gmail.com>
2017-06-05 23:06:05 +0700

    First Commit

---
+++ Lexical Analyzer/Form1.Designer.vb
@@ -0,0 +1,136 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnGenerate = New System.Windows.Forms.Button()
+        Me.lblToken = New System.Windows.Forms.Label()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.BtnValid = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.SuspendLayout()
+        '
+        'btnGenerate
+        '
+        Me.btnGenerate.Location = New System.Drawing.Point(132, 191)
+        Me.btnGenerate.Name = "btnGenerate"
+        Me.btnGenerate.Size = New System.Drawing.Size(140, 23)
+        Me.btnGenerate.TabIndex = 0
+        Me.btnGenerate.Text = "Generate Token"
+        Me.btnGenerate.UseVisualStyleBackColor = True
+        '
+        'lblToken
+        '
+        Me.lblToken.AutoSize = True
+        Me.lblToken.Location = New System.Drawing.Point(13, 94)
+        Me.lblToken.Name = "lblToken"
+        Me.lblToken.Size = New System.Drawing.Size(10, 13)
+        Me.lblToken.TabIndex = 1
+        Me.lblToken.Text = "-"
+        '
+        'txtInput
+        '
+        Me.txtInput.Location = New System.Drawing.Point(16, 50)
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(256, 20)
+        Me.txtInput.TabIndex = 2
+        '
+        'BtnValid
+        '
+        Me.BtnValid.Location = New System.Drawing.Point(132, 223)
+        Me.BtnValid.Name = "BtnValid"
+        Me.BtnValid.Size = New System.Drawing.Size(140, 23)
+        Me.BtnValid.TabIndex = 3
+        Me.BtnValid.Text = "Validasi"
+        Me.BtnValid.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(17, 136)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(19, 25)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "-"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(16, 31)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(37, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Input: "
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(16, 78)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(44, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Token: "
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(16, 123)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(72, 13)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Hasil Validasi:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 260)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.BtnValid)
+        Me.Controls.Add(Me.txtInput)
+        Me.Controls.Add(Me.lblToken)
+        Me.Controls.Add(Me.btnGenerate)
+        Me.Name = "Form1"
+        Me.Text = "Lexical Analyzer"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnGenerate As Button
+    Friend WithEvents lblToken As Label
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents BtnValid As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+End Class
---
+++ Lexical Analyzer/Form1.vb
@@ -0,0 +1,478 @@
+﻿Public Class Form1
+    Dim aToken(0) As Integer
+    Dim token As String
+    Private Sub btnGenerate_Click(sender As Object, e As EventArgs) Handles btnGenerate.Click
+        Dim sText As String ' sText stands for stringText - Deklarasi Variabel
+        Dim i, length As Integer ' Deklarasi Variabel
+        sText = txtInput.Text + " " 'Memeasukan isi TextBoxInput ke variabel string & fungsi ditambah spasi sebagai penanda agar token keluar karena setiap token dikeluarkan bila sText.chars(i) = " " 
+        sText = sText.ToLower ' << set ke lower agar tidak case sensitive
+        length = sText.Length
+        i = 0
+
+        lblToken.Text = ""
+        token = ""
+        'Public aToken(0) As Integer
+        Dim j As Integer
+        j = -1
+
+        Dim stack As New Stack()
+Space:
+        'On Error Resume Next
+        If i < length Then
+            Select Case sText.Chars(i)
+                Case "p", "q", "r", "s" ' PQRS
+                    i = i + 1
+                    Select Case sText.Chars(i)
+                        Case " "
+
+                            i = i + 1
+                            token = token + " 1" ' Token dikeluarkan saat sText.chars(i) = " " agar mengetahui apakan suatu sintaks tidak ada lanjutannya, misal ifg agar menghasilkan error bukan 6error
+
+                            j = j + 1
+                            ReDim Preserve aToken(j)
+                            aToken(aToken.GetUpperBound(0)) = 1
+                            GoTo Space
+                        Case Else
+                            token = token + "error"
+                    End Select
+                Case "n" ' NOT
+                    i = i + 1
+                    Select Case sText.Chars(i)
+                        Case "o"
+                            i = i + 1
+                            Select Case sText.Chars(i)
+                                Case "t"
+                                    i = i + 1
+                                    Select Case sText.Chars(i)
+                                        Case " "
+                                            token = token + " 2"
+                                            i = i + 1
+
+                                            j = j + 1
+                                            ReDim Preserve aToken(j)
+                                            aToken(aToken.GetUpperBound(0)) = 2
+
+                                            GoTo Space
+                                        Case Else
+                                            token = token + "error"
+                                    End Select
+                                Case Else
+                                    token = token + "error"
+                            End Select
+                        Case Else
+                            token = token + "error"
+                    End Select
+                Case "a" ' AND
+                    i = i + 1
+                    Select Case sText.Chars(i)
+                        Case "n"
+                            i = i + 1
+                            Select Case sText.Chars(i)
+                                Case "d"
+                                    i = i + 1
+                                    Select Case sText.Chars(i)
+                                        Case " "
+                                            token = token + " 3"
+                                            i = i + 1
+
+
+                                            j = j + 1
+                                            ReDim Preserve aToken(j)
+                                            aToken(aToken.GetUpperBound(0)) = 3
+                                            GoTo Space
+                                        Case Else
+                                            token = token + "error"
+                                    End Select
+                                Case Else
+                                    token = token + "error"
+                            End Select
+                        Case Else
+                            token = token + "error"
+                    End Select
+                Case "o" ' OR
+                    i = i + 1
+                    Select Case sText.Chars(i)
+                        Case "r"
+                            i = i + 1
+                            Select Case sText.Chars(i)
+                                Case " "
+                                    i = i + 1
+                                    token = token + " 4"
+
+
+                                    j = j + 1
+                                    ReDim Preserve aToken(j)
+                                    aToken(aToken.GetUpperBound(0)) = 4
+                                    GoTo Space
+                                Case Else
+                                    token = token + "error"
+                            End Select
+                        Case Else
+                            token = token + "error"
+                    End Select
+                Case "x" ' XOR
+                    i = i + 1
+                    Select Case sText.Chars(i)
+                        Case "o"
+                            i = i + 1
+                            Select Case sText.Chars(i)
+                                Case "r"
+                                    i = i + 1
+                                    Select Case sText.Chars(i)
+                                        Case " "
+                                            i = i + 1
+                                            token = token + " 5"
+
+
+                                            j = j + 1
+                                            ReDim Preserve aToken(j)
+                                            aToken(aToken.GetUpperBound(0)) = 5
+                                            GoTo Space
+                                        Case Else
+                                            token = token + "error"
+                                    End Select
+                                Case Else
+                                    token = token + "error"
+                            End Select
+                        Case Else
+                            token = token + "error"
+                    End Select
+                Case "i" ' IF
+                    i = i + 1
+                    Select Case sText.Chars(i)
+                        Case "f"
+                            i = i + 1
+                            Select Case sText.Chars(i)
+                                Case " "
+                                    i = i + 1
+                                    token = token + " 6"
+
+                                    j = j + 1
+                                    ReDim Preserve aToken(j)
+                                    aToken(aToken.GetUpperBound(0)) = 6
+                                    GoTo Space
+                                Case "f"
+                                    i = i + 1
+                                    Select Case sText.Chars(i)
+                                        Case " "
+                                            i = i + 1
+                                            token = token + " 8"
+
+
+                                            j = j + 1
+                                            ReDim Preserve aToken(j)
+                                            aToken(aToken.GetUpperBound(0)) = 8
+                                            GoTo Space
+                                        Case Else
+                                            token = token + "error"
+                                    End Select
+                                Case Else
+                                    token = token + "error"
+                            End Select
+                        Case Else
+                            token = token + "error"
+                    End Select
+                Case "t" ' THEN
+                    i = i + 1
+                    Select Case sText.Chars(i)
+                        Case "h"
+                            i = i + 1
+                            Select Case sText.Chars(i)
+                                Case "e"
+                                    i = i + 1
+                                    Select Case sText.Chars(i)
+                                        Case "n"
+                                            i = i + 1
+                                            token = token + " 7"
+
+
+                                            j = j + 1
+                                            ReDim Preserve aToken(j)
+                                            aToken(aToken.GetUpperBound(0)) = 7
+
+                                            Select Case sText.Chars(i)
+                                                Case " "
+                                                    i = i + 1
+                                                    GoTo Space
+                                                Case Else
+                                                    token = token + "error"
+                                            End Select
+                                        Case Else
+                                            token = token + "error"
+                                    End Select
+                                Case Else
+                                    token = token + "error"
+                            End Select
+                        Case Else
+                            token = token + "error"
+                    End Select
+                Case "(" ' GROUPING
+                    i = i + 1
+                    Select Case sText.Chars(i)
+                        Case " "
+                            token = token + " 9"
+                            i = i + 1
+                            j = j + 1
+                            ReDim Preserve aToken(j)
+                            aToken(aToken.GetUpperBound(0)) = 9
+                            GoTo Space
+                        Case Else
+                            token = token + "error"
+                    End Select
+
+                Case ")" ' GROUPING JAGA- JAGA KALAU ADA GROUPING SETELAH GROUPING
+                    i = i + 1
+                    Select Case sText.Chars(i)
+                        Case " "
+                            token = token + " 10"
+                            i = i + 1
+                            j = j + 1
+                            ReDim Preserve aToken(j)
+                            aToken(aToken.GetUpperBound(0)) = 10
+                            GoTo Space
+                        Case Else
+                            token = token + "error"
+                    End Select
+                Case " "
+                    token = token
+                Case Else
+                    token = token + "error"
+            End Select
+        End If
+        stack.Push(1)
+        lblToken.Text = token 'Set LabelToken menjadi token
+
+    End Sub
+
+    Dim iV As Integer  ' iV = index untuk validasi
+    Dim Stack As New Stack()
+    Dim Valid As Boolean
+
+    Private Sub BtnValid_Click(sender As Object, e As EventArgs) Handles BtnValid.Click
+        If token.Contains("error") Then
+            Label1.Text = "ERROR"
+            Label1.ForeColor = Color.Red
+        Else
+            Stack.Push(99) '# diganti jadi 99 karena nanti int tidak bisa dibandingkan dengan char
+            iV = -1
+            q0()
+            If Valid = True Then
+                Label1.Text = "VALID"
+                Label1.ForeColor = Color.Green
+            Else
+                Label1.Text = "TIDAK VALID"
+                Label1.ForeColor = Color.Red
+            End If
+        End If
+
+
+    End Sub
+
+    Sub q0()
+        iV = iV + 1
+        If iV <= aToken.GetUpperBound(0) Then
+            Select Case aToken(iV)
+                Case 1
+                    Stack.Push(1)
+                    q1()
+                Case 2
+                    q3()
+                Case 6
+                    Stack.Push(6)
+                    q3()
+                Case 9
+                    Stack.Push(9)
+                    q0()
+                Case Else
+                    Valid = False
+            End Select
+        Else
+            Valid = False
+        End If
+    End Sub
+
+    Sub q1()
+        iV = iV + 1
+        If iV <= aToken.GetUpperBound(0) Then
+            If aToken(iV) = 1 Then
+                If Stack.Peek = 1 Then
+                    Stack.Pop()
+                    If Stack.Peek = 9 Then
+                        Stack.Pop()
+                        q6()
+                    Else
+                        Valid = False
+                    End If
+                Else
+                    Valid = False
+                End If
+            ElseIf aToken(iV) = 10 Then
+                If Stack.Peek = 1 Then
+                    Stack.Pop()
+                    If Stack.Peek = 9 Then
+                        Stack.Pop()
+                        q6()
+                    Else
+                        Valid = False
+                    End If
+                End If
+            ElseIf aToken(iV) = 7 Then
+                If Stack.Peek = 6 Then
+                    Stack.Pop()
+                    q4()
+                Else
+                    Valid = False
+                End If
+            ElseIf aToken(iV) <> 3 And aToken(iV) <> 4 And aToken(iV) <> 5 And aToken(iV) <> 8 Then
+                Valid = False
+            ElseIf Stack.Peek = 1 Then
+                Stack.Pop()
+                q5()
+            Else
+                Valid = False
+            End If
+        ElseIf Stack.Peek = 1 Then
+            Stack.Pop()
+            q8()
+        Else
+            Valid = False
+        End If
+    End Sub
+
+    Sub q2()
+        iV = iV + 1
+        If iV <= aToken.GetUpperBound(0) Then
+            Select Case aToken(iV)
+                Case 1
+                    Stack.Push(1)
+                    q1()
+                Case 9
+                    Stack.Push(9)
+                    q0()
+                Case Else
+                    Valid = False
+            End Select
+        Else
+            Valid = False
+        End If
+    End Sub
+
+    Sub q3()
+        iV = iV + 1
+
+        If iV <= aToken.GetUpperBound(0) Then
+            Select Case aToken(iV)
+                Case 1
+                    q1()
+                Case 9
+                    Stack.Push(9)
+                    q0()
+                Case Else
+                    Valid = False
+            End Select
+        Else
+            Valid = False
+        End If
+    End Sub
+
+    Sub q4()
+        iV = iV + 1
+
+        If iV <= aToken.GetUpperBound(0) Then
+            Select Case aToken(iV)
+                Case 1
+                    q8()
+                Case 2
+                    q2()
+                Case 9
+                    Stack.Push(9)
+                    q0()
+                Case Else
+                    Valid = False
+            End Select
+        Else
+            Valid = False
+        End If
+    End Sub
+
+    Sub q5()
+
+        iV = iV + 1
+        If iV <= aToken.GetUpperBound(0) Then
+            Select Case aToken(iV)
+                Case 1
+                    Stack.Push(1)
+                    q1()
+                Case 2
+                    q2()
+                Case 9
+                    Stack.Push(9)
+                    q0()
+                Case Else
+                    Valid = False
+            End Select
+        Else
+            Valid = False
+        End If
+    End Sub
+
+    Sub q6()
+        iV = iV + 1
+
+        If iV <= aToken.GetUpperBound(0) Then
+            Select Case aToken(iV)
+                Case 10
+                    If Stack.Peek = 9 Then
+                        Stack.Pop()
+                        q6()
+                    Else
+                        Valid = False
+                    End If
+                Case 7
+                    If Stack.Peek = 6 Then
+                        Stack.Pop()
+                        q4()
+                    Else
+                        Valid = False
+                    End If
+            End Select
+        ElseIf Stack.Peek = 99 Then
+            Stack.Pop()
+            Valid = True
+        Else
+            Valid = False
+        End If
+    End Sub
+
+    Sub q7()
+        iV = iV + 1
+
+        If iV <= aToken.GetUpperBound(0) Then
+            Select Case aToken(iV)
+                Case 10
+                    If Stack.Peek = 9 Then
+                        Stack.Pop()
+                        q6()
+                    Else
+                        Valid = False
+                    End If
+                Case Else
+                    Valid = False
+            End Select
+        Else
+            Valid = False
+        End If
+    End Sub
+
+    Sub q8()
+        iV = iV + 1
+        If iV <= aToken.GetUpperBound(0) Then
+            Valid = False
+        ElseIf Stack.Peek = 99 Then
+            Stack.Pop()
+            Valid = True
+        Else
+            Valid = False
+        End If
+    End Sub
+End Class
